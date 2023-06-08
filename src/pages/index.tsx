027e1fa09f5465a547d49102bcc853a81d087949
Miguel Amores <migueamores@gmail.com>
2023-06-08 09:06:33 -0500

    get tweets list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,15 @@ const Home: NextPage = () => {
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
       </header>
       <NewTweetForm />
+      <RecentTweets />
     </>
   );
 };
+
+function RecentTweets() {
+  const tweets = [];
+
+  return <InfiniteTweetList tweets={tweets} />;
+}
 
 export default Home;
