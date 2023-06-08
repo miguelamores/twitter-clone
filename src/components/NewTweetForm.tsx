946e8393955875c24b08d880bf8bd4a2d4ffc906
Miguel Amores <migueamores@gmail.com>
2023-06-08 07:56:09 -0500

    add route and mutation to create tweets

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,7 +1,14 @@
-import { useState, useLayoutEffect, useRef, useCallback } from "react";
+import {
+  useState,
+  useLayoutEffect,
+  useRef,
+  useCallback,
+  type FormEvent,
+} from "react";
 import { Button } from "./Button";
 import { ProfileImage } from "./ProfileImage";
 import { useSession } from "next-auth/react";
+import { api } from "~/utils/api";
 
 function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
   if (!textArea) return;
@@ -30,10 +37,25 @@ function Form() {
     updateTextAreaSize(textAreaRef.current);
   }, [inputValue]);
 
+  const createTweet = api.tweet.create.useMutation({
+    onSuccess: () => {
+      setInputValue("");
+    },
+  });
+
   if (session.status !== "authenticated") return null;
 
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    createTweet.mutate({ content: inputValue });
+  }
+
   return (
-    <form className="flex flex-col gap-2 border-b px-4 py-2">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 border-b px-4 py-2"
+    >
       <div className="flex gap-4">
         <ProfileImage src={session.data?.user?.image} />
         <textarea
