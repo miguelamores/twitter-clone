dd6e7159934ac41e82d2406c160e9e868a514354
Miguel Amores <migueamores@gmail.com>
2023-06-28 17:14:05 -0500

    add revalidation

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -53,6 +53,9 @@ export const profileRouter = createTRPCRouter({
         addedFollow = false;
       }
 
+      void ctx.revalidateSSG?.(`/profiles/${userId}`);
+      void ctx.revalidateSSG?.(`/profiles/${currentUserId}`);
+
       return { addedFollow };
     }),
 });
